dbebe514886bf3d6c9ff45abdf7a197d4e3db43e
Marvin Laibold <marvin@laibold.com>
2021-06-28 20:52:28 +0200

    client: add javafx main

--- client/src/main/java/de/hs_rm/chat_client/Main.java
+++ client/src/main/java/de/hs_rm/chat_client/Main.java
@@ -1,8 +1,24 @@
 package de.hs_rm.chat_client;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        var l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var scene = new Scene(new StackPane(l), 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello chatters?");
+        launch();
     }
 }
