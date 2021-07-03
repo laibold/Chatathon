b59118aea6e1270f45e71f1df1b9a009ed6794ec
Marvin Laibold <marvin@laibold.com>
2021-07-03 13:48:33 +0200

    client: add MessageGenerator and PasswordHasher

--- client/src/main/java/de/hs_rm/chat_client/controller/SignupController.java
+++ client/src/main/java/de/hs_rm/chat_client/controller/SignupController.java
@@ -1,5 +1,11 @@
 package de.hs_rm.chat_client.controller;
 
+import de.hs_rm.chat_client.model.header.InvalidHeaderException;
+import de.hs_rm.chat_client.model.header.MessageType;
+import de.hs_rm.chat_client.model.user.User;
+import de.hs_rm.chat_client.service.MessageGenerator;
+import de.hs_rm.chat_client.service.PasswordHasher;
+import de.hs_rm.chat_client.service.SendMessageService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -16,6 +22,8 @@ public class SignupController extends BaseController {
     @FXML
     private PasswordField passwordText;
 
+    SendMessageService sendMessageService;
+
     @FXML
     public void initialize() {
 
@@ -26,7 +34,17 @@ public class SignupController extends BaseController {
         if (usernameText.getText().isBlank() || passwordText.getText().isBlank()) {
             new Alert(Alert.AlertType.NONE, "Eingaben überprüfen!", ButtonType.CLOSE).showAndWait();
         } else {
-            System.out.println("sign up");
+            var username = usernameText.getText().trim();
+            var password = PasswordHasher.getHashedPassword(passwordText.getText().trim());
+
+            var message = "";
+            try {
+                message = MessageGenerator.generateMessage(MessageType.SIGN_UP, new User(username, password));
+            } catch (InvalidHeaderException e) {
+                new Alert(Alert.AlertType.ERROR, "Allgemeiner Fehler!", ButtonType.CLOSE).showAndWait();
+            }
+
+            //sendMessageService.sendMessage(message);
         }
     }
 
