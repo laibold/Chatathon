94da151e12320ec1d58615a7b8fcbe4f9567ab20
Raza Ahmed <raza.ahmed@aoe.com>
2021-07-03 16:07:50 +0200

    Fix Server not responding to following requests

--- server/src/main/java/de/hs_rm/chat_server/communication/Listener.java
+++ server/src/main/java/de/hs_rm/chat_server/communication/Listener.java
@@ -5,6 +5,7 @@ import de.hs_rm.chat_server.service.HeaderMapper;
 
 import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Listener {
     private ServerSocket serverSocket;
@@ -36,6 +37,8 @@ public class Listener {
                         // Parse header to get body length
                         var headerString = reader.readLine();
 
+                        System.out.println(reader.readLine());
+
                         // TODO: Abfangen: if (line == null || line.equals(""))
                         var header = HeaderMapper.toHeader(headerString);
                         var chars = new char[header.getContentLength()];
@@ -48,6 +51,8 @@ public class Listener {
                             body = "";
                         }
 
+                        handleRequests(socket, reader, writer);
+
                         // TODO: Forward message type and body to MessageHandler
 
 
@@ -58,6 +63,25 @@ public class Listener {
                 thread.start();
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleRequests(final Socket socket, final BufferedReader reader, final BufferedWriter writer) {
+        while (true) {
+            final String line;
+
+            try {
+
+                line = reader.readLine();
+                if (line == null || line.equals(""))
+                    return;
+                System.out.println("INCOMING (" + socket.getRemoteSocketAddress().toString() + "):\t" + line);
+                writer.flush();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+
+                return;
             }
         }
     }
