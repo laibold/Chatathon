8d755be63942849d13b5613091203635b1df800b
Raza Ahmed <raza.ahmed@aoe.com>
2021-07-04 19:10:44 +0200

    Refactor OutgoingChatRequestResponseMessageHandler

--- server/src/main/java/de/hs_rm/chat_server/communication/handler/OutgoingChatRequestResponseMessageHandler.java
+++ server/src/main/java/de/hs_rm/chat_server/communication/handler/OutgoingChatRequestResponseMessageHandler.java
@@ -2,6 +2,11 @@ package de.hs_rm.chat_server.communication.handler;
 
 import de.hs_rm.chat_server.communication.MessageGenerator;
 import de.hs_rm.chat_server.model.message.*;
+import de.hs_rm.chat_server.service.ClientService;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class OutgoingChatRequestResponseMessageHandler extends MessageHandler {
     @Override
@@ -20,11 +25,32 @@ public class OutgoingChatRequestResponseMessageHandler extends MessageHandler {
             finalChatRequestResponse.setPort(message.getClient().getPort());
         }
 
+        var clientService = ClientService.getInstance();
+        var clientSocket = clientService.getClient(response.getUsername()).getSocket();
+        BufferedWriter outToClient = null;
+
         try {
-            return MessageGenerator.generateMessage(status, MessageType.FINAL_CHAT_REQUEST_RESPONSE, finalChatRequestResponse);
+            outToClient = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String outgoingRequest = null;
+
+        try {
+            outgoingRequest = MessageGenerator.generateMessage(status, MessageType.FINAL_CHAT_REQUEST_RESPONSE, finalChatRequestResponse);
         } catch (InvalidHeaderException e) {
             e.printStackTrace();
         }
+
+        System.out.println("OUTGOING (" + clientSocket.getRemoteSocketAddress().toString() + "):\t" + outgoingRequest);
+        try {
+            outToClient.write(outgoingRequest + "\n");
+            outToClient.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         return null;
     }
