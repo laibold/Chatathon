1e373243e3b55ace7759e926baca5ab4a537c245
Marvin Laibold <marvin@laibold.com>
2021-07-12 22:46:49 +0200

    adapt MMS

--- client/src/main/java/de/hs_rm/chat_client/config/Config.java
+++ client/src/main/java/de/hs_rm/chat_client/config/Config.java
@@ -8,6 +8,6 @@ public class Config {
     public static final String SERVER_IP_ADDRESS = "localhost";
     public static final int SERVER_PORT = 8080;
 
-    public static final int MAXIMUM_UDP_SEGMENT_SIZE = 4;
+    public static final int MAXIMUM_UDP_SEGMENT_SIZE = 20;
     public static final double UDP_ERROR_RATE = 0.1;
 }
